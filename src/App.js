b4305efae9d12b4cbea191d6da3fcc4a1aa3a05e
Naresh28696 <nareshpatil.nrp@gmail.com>
2020-03-26 11:35:47 +0530

    [Naresh] Modify. add dashboard route in git status

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { Switch } from 'react-router-dom'
 import Registration from './components/registration'
 import Login from './components/login'
+import Dashboard from './components/dashboard'
 import ForgotPassword from './components/forgotPassword'
 import FundooServices from './components/fundooSevices'
 
@@ -14,9 +15,9 @@ function App() {
         <Switch>
           <Route path='/registration' component={Registration} />
           <Route path='/login' component={Login} />
-          {/* <Route path='/dashboard' component={Dashboard} /> */}
+          <Route path='/dashboard' component={Dashboard} />
           <Route path='/forgotpassword' component={ForgotPassword} />
-          <Route path="/fundooServices" component={FundooServices} />
+          <Route path="/" component={FundooServices} />
         </Switch>
       </Router>
   );
